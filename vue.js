1be81e00500a33d48e03b5caf7c8479fe62f11a0
Kosan3 <ichiro.neko.0421@gmail.com>
2020-08-22 16:59:53 +0900

    kiso

--- vue.js
+++ vue.js
@@ -1,7 +1,12 @@
-console.assert(typeof Vue !== 'undefined');
-new Vue({
+var items = [
+  {name:'鉛筆', price:300, quantity:0},
+  {name:'ノート', price:400, quantity:0},
+  {name:'消しゴム', price:500, quantity:0}
+]
+
+var vm = new Vue({
   el: '#app',
   data: {
-    message: 'こんにちは！'
+    items: items
   }
-});
+})
