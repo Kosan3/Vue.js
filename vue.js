befeef5b6333737a8c54f02794eadf02a3dace57
Kosan3 <ichiro.neko.0421@gmail.com>
2020-09-08 23:55:52 +0900

    reduceメソッドの使用方法を理解

--- vue.js
+++ vue.js
@@ -1,8 +1,44 @@
-var items = [{name:'たこ焼き', price:300, quantity:0}, {name:'焼きそば', price:250, quantity:0}];
+var items = [
+  {
+    name: '鉛筆',
+    price: 300,
+    quantity: 1
+  },
+  {
+    name: 'ノート',
+    price: 400,
+    quantity: 1
+  },
+  {
+    name: '消しゴム',
+    price: 500,
+    quantity: 1
+  }
+]
 
-var vm = new Vue ({
-  el: "#app",
+var vm = new Vue({
+  el: '#app',
   data: {
     items: items
+  },
+  filters: {
+    numberWithDelimiter: function(value) {
+      if (!value) {
+        return '0'
+      }
+      return value.toString().replace(/(\d)(?=(\d{3})+$)/g, '$1,')
+    }
+  },
+  computed: {
+    totalPrice: function() {
+      return this.items.reduce(function(prev, current, index, array) {
+        console.log(++index + '回目  ' + current.price);
+        return prev + (current.price * current.quantity);
+      }, 0)
+    }
+    ,
+    totalPriceWithTax: function() {
+      return Math.floor(this.totalPrice * 1.1)
+    }
   }
 })
