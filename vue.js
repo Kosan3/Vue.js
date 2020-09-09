fa2c9bf45731a74d5636a75c66ae98bd18c00764
Kosan3 <ichiro.neko.0421@gmail.com>
2020-09-09 17:11:08 +0900

    2.9

--- vue.js
+++ vue.js
@@ -1,44 +1,38 @@
-var items = [
+let items = [
   {
-    name: '鉛筆',
-    price: 300,
-    quantity: 1
-  },
-  {
-    name: 'ノート',
-    price: 400,
-    quantity: 1
-  },
-  {
-    name: '消しゴム',
-    price: 500,
-    quantity: 1
+    name: '鉛筆', price: 300, quantity: 1
+  }, {
+    name: 'ノート', price: 400, quantity: 1
+  }, {
+    name: '消しゴム', price: 500, quantity: 1
   }
 ]
 
-var vm = new Vue({
+let vm = new Vue({
   el: '#app',
   data: {
     items: items
   },
   filters: {
     numberWithDelimiter: function(value) {
+      console.log(!value);
       if (!value) {
-        return '0'
+        return '0';
       }
-      return value.toString().replace(/(\d)(?=(\d{3})+$)/g, '$1,')
+      return value.toString().replace(/(\d)(?=(\d{3})+$)/g, '$1,');
     }
   },
   computed: {
     totalPrice: function() {
       return this.items.reduce(function(prev, current, index, array) {
-        console.log(++index + '回目  ' + current.price);
         return prev + (current.price * current.quantity);
       }, 0)
-    }
-    ,
+    },
     totalPriceWithTax: function() {
-      return Math.floor(this.totalPrice * 1.1)
+      return Math.floor(this.totalPrice * 1.1);
+    },
+    canBuy: function() {
+      return this.totalPrice <= 2000;
     }
   }
 })
