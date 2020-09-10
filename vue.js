fb876fff8f896eef45a949f65446439b1ffcc760
Kosan3 <ichiro.neko.0421@gmail.com>
2020-09-10 17:48:27 +0900

    [finish]3-3

--- vue.js
+++ vue.js
@@ -11,33 +11,52 @@ let items = [
 let vm = new Vue({
   el: '#app',
   data: {
-    items: items
+    items: items,
+    itemName: 'product'
   },
   filters: {
     numberWithDelimiter: function(value) {
       if (!value) {
-        return '0';
+        return 0
+      } else {
+        return value.toString().replace(/(\d)(?=(\d{3})+$)/g, '$1,')
       }
-      return value.toString().replace(/(\d)(?=(\d{3})+$)/g, '$1,');
+    }
+  },
+  methods: {
+    doBuy: function() {
+      alert(this.totalPriceWithTax + '円のお買い上げです!')
+      this.items.forEach(function(item) {
+        item.quantity = 0
+      })
+    },
+    hey: function(event) {
+      console.log(event.target.value)
     }
   },
   computed: {
     totalPrice: function() {
-      return this.items.reduce(function(prev, current, index, array) {
-        return prev + (current.price * current.quantity);
+      return this.items.reduce(function(prev, current) {
+        return prev + (current.price * current.quantity)
       }, 0)
     },
     totalPriceWithTax: function() {
-      return Math.floor(this.totalPrice * 1.1);
+      return Math.floor(this.totalPrice * 1.1)
     },
     canBuy: function() {
-      return this.totalPrice >= 1000;
+      return this.totalPrice <= 1000
     },
     errorMessageStyle: function() {
       return {
-        border: this.canBuy ? '' : '1px solid red',
-        color: this.canBuy ? '' : 'red'
+        border: this.canBuy ? '1px solid red' : '',
+        color: this.canBuy ? 'red' : ''
       }
+    },
+    errorText: function() {
+      return this.item.quantity == '0'
     }
+  },
+  mounted: function() {
+    let app = this.$el
   }
 })
