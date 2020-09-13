b843a282660dbc150c6bb873801454c08b99221b
Kosan3 <ichiro.neko.0421@gmail.com>
2020-09-13 11:41:54 +0900

    4.finish

--- Chapter4/vue.js
+++ Chapter4/vue.js
@@ -1,52 +1,202 @@
-var getUsers = function(callback) {
-  setTimeout(function() {
-    callback(null, [
-      {
-        id: 1,
-        name: 'Yuya Kosaka'
-      },
-      {
-        id: 2,
-        name: 'Chinami Ueno'
-      }
-    ])
+var getUsers = function (callback) {
+  setTimeout(function () {
+    callback(null, userData)
   }, 1000)
 }
 
-var UserList = {
-  template: '#user-list',
+var userData = [
+  {
+    id: 1,
+    name: 'Yuya Kosak',
+    description: '埼玉県出身です'
+  },
+  {
+    id: 2,
+    name: 'Chinami Ueno',
+    description: '兵庫県出身です'
+  }
+]
+
+var getUser = function(userId, callback) {
+  setTimeout(function() {
+    var filteredUsers = userData.filter(function(user) {
+      return user.id === parseInt(userId, 10)
+    })
+    callback(null, filteredUsers && filteredUsers[0])
+  }, 1000)
+}
+
+var UserDetail = {
+  template: '#user-detail',
   data: function() {
     return {
       loading: false,
-      users: function() {
-        return []
-      },
+      user: null,
       error: null
     }
   },
-
   created: function() {
     this.fetchData()
   },
-
   watch: {
     '$route': 'fetchData'
   },
-
   methods: {
     fetchData: function() {
       this.loading = true
-      getUsers((function(err, users) {
+      getUser(this.$route.params.userId, (function(err, user) {
         this.loading = false
-        if (err) {
+        if(err) {
           this.error = err.toString()
         } else {
-          this.users = users
+          this.user = user
         }
       }).bind(this))
     }
   }
 }
+
+var UserList = {
+      template: '#user-list',
+      data: function () {
+        return {
+          loading: false,
+          users: function () {
+            return []
+          },
+          error: null
+        }
+      },
+
+      created: function () {
+        this.fetchData()
+      },
+
+      watch: {
+        '$route': 'fetchData'
+      },
+
+      methods: {
+        fetchData: function () {
+          this.loading = true
+          getUsers((function (err, users) {
+            this.loading = false
+            if (err) {
+              this.error = err.toString()
+            } else {
+              this.users = users
+            }
+          }).bind(this))
+        }
+      }
+    }
+
+
+var postUser = function (params, callback) {
+  setTimeout(function () {
+    // idは追加されるごとに自動的にincrementされていく
+    params.id = userData.length + 1
+    userData.push(params)
+    callback(null, params)
+  }, 1000)
+}
+
+var UserCreate = {
+  template: '#user-create',
+  data: function () {
+    return {
+      sending: false,
+      user: this.defaultUser(),
+      error: null
+    }
+  },
+
+  created: function () {
+  },
+
+  methods: {
+    defaultUser: function () {
+      return {
+        name: '',
+        description: ''
+      }
+    },
+
+    createUser: function () {
+      // 入力パラメーターのバリデーション
+      if (this.user.name.trim() === '') {
+        this.error = 'Nameは必須です'
+        return
+      }
+      if (this.user.description.trim() === '') {
+        this.error = 'Descriptionは必須です'
+        return
+      }
+      postUser(this.user, (function (err, user) {
+        this.sending = false
+        if (err) {
+          this.error = err.toString()
+        } else {
+          this.error = null
+          // デフォルトでフォームをリセット
+          this.user = this.defaultUser()
+          alert('新規ユーザーが登録されました')
+          // ユーザー一覧ページに戻る
+          this.$router.push('/users')
+        }
+      }).bind(this))
+    }
+  }
+}
+
+var Auth = {
+  login: function (email, pass, cb) {
+    // ダミーデータを使った擬似ログイン
+    setTimeout(function () {
+      if (email === 'vue@example.com' && pass === 'vue') {
+        // ログイン成功時はローカルストレージにtokenを保存する
+        localStorage.token = Math.random().toString(36).substring(7)
+        if (cb) { cb(true) }
+      } else {
+        if (cb) { cb(false) }
+      }
+    }, 0)
+  },
+
+  logout: function () {
+    delete localStorage.token
+  },
+
+  loggedIn: function () {
+    // ローカルストレージにtokenがあればログイン状態とみなす
+    return !!localStorage.token
+  }
+}
+
+var Login = {
+  template: "#login",
+  data: function() {
+    return {
+      email: 'vue@example.com',
+      pass: '',
+      error: false
+    }
+  },
+  methods: {
+    login: function() {
+      Auth.login(this.email, this.pass, (function (loggedIn) {
+        if (!loggedIn) {
+          this.error = true
+        } else {
+          this.$router.replace(this.$route.query.redirect || '/')
+        }
+      }).bind(this))
+    }
+  }
+}
+
+
+
 
 var router = new VueRouter({
   routes: [
@@ -59,16 +209,47 @@ var router = new VueRouter({
     {
       path: '/users',
       component: UserList
+    },
+    {
+      path: '/users/new',
+      component: UserCreate,
+      beforeEnter: function(to, from, next) {
+        if (!Auth.loggedIn()) {
+          next({
+            path: '/login',
+            query: { redirect: to.fullPath }
+          })
+        } else {
+          next()
+        }
+      }
+    },
+    {
+      path: '/users/:userId',
+      component: UserDetail
+    },
+    {
+      path: '/login',
+      component: Login
+    },
+    {
+      path: '/logout',
+      beforeEnter: function(to, from, next) {
+        Auth.logout()
+        next('/top')
+      }
+    },
+    {
+      path: '*',
+      redirect: '/top'
     }
   ]
 })
 
 
-
-
-
-
-
 var app = new Vue({
+  data: {
+    Auth: Auth
+  },
   router: router
 }).$mount('#app')
