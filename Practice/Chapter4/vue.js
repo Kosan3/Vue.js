651b72887aaa062dabf3696262fd97a7a736407d
Kosan3 <ichiro.neko.0421@gmail.com>
2020-09-25 17:50:10 +0900

    chpter5start

--- Practice/Chapter4/vue.js
+++ Practice/Chapter4/vue.js
@@ -1,17 +1,29 @@
-let myBook = {
-  data: function() {
-    return {
-      book: {
-        isbn: '978-4-3234-2111',
-        title: 'ワクワクさんの心情',
-        price: 3800,
-        publish: 'ホグワーツ'
-      }
-    }
-  },
+let bannerMember = {
   template: `
-    <div>
-      <slot :book="book">{{book.title}}  ({{book.publish}})</slot>
+    <div class="banner">
+      <h3>WINGSメンバー募集中！</h3>
+      <p>あなたもプロジェクトに参加しませんか？<br>
+      現在、仲間を探しています。</p>
+    </div>
+  `
+};
+
+let bannerNew = {
+  template: `
+    <div class="banner">
+      <h3>新刊「HTML5超入門 」発売</h3>
+      <p>ステップバイステップで学ぶ入門書です。<br />
+        HTML5アプリの基礎知識、画面のデザイン、コードの書き方などが理解できます。</p>
+    </div>
+  `
+};
+
+let bannerEnv = {
+  template: `
+    <div class="banner">
+      <h3>環境構築設定</h3>
+      <p>開発環境の設定方法を図を交えて詳しく解説します。<br />
+        紹介している各モジュールは、日々頻繁にバージョンアップが行われています。</p>
     </div>
   `
 };
@@ -19,6 +31,26 @@ let myBook = {
 new Vue({
   el: '#app',
   components: {
-    'my-book': myBook
+    'banner-member': bannerMember,
+    'banner-new': bannerNew,
+    'banner-env': bannerEnv
+  },
+  created: function() {
+    let that = this;
+    this.interval = setInterval(function() {
+      that.current = (that.current + 1) % that.components.length;
+    }, 3000);
+  },
+  beforeDestroy: function() {
+    clearInterval(this.interval);
+  },
+  computed: {
+    currentBanner: function() {
+      return 'banner-' + this.components[this.current];
+    }
+  },
+  data: {
+    current: 0,
+    components: ['member', 'new', 'env']
   }
 });
