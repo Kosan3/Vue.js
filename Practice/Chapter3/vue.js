343940515ad297ee200f1237e913051c2b249dce
Kosan3 <ichiro.neko.0421@gmail.com>
2020-09-27 14:07:27 +0900

    107

--- Practice/Chapter3/vue.js
+++ Practice/Chapter3/vue.js
@@ -1,26 +1,13 @@
 new Vue({
   el: '#app',
   data: {
-    books: [
-      {
-        title: 'Javascript',
-        price: 1200
-      },
-      {
-        title: 'Ruby',
-        price: 900
-      },
-      {
-        title: 'Java',
-        price: 1300
-      }
+    lists: [
+      '赤', '青', '黄'
     ]
   },
-  computed: {
-    expensiveBooks: function() {
-      return this.books.filter(function(book) {
-        return book.price < 1000;
-      })
+  methods: {
+    onclick: function() {
+      this.lists.shift()
     }
   }
-})
+});
