f3d011488840c3ec5d61c5c7b0fd0560917db0b8
Kosan3 <ichiro.neko.0421@gmail.com>
2020-09-27 17:29:06 +0900

    3finish

--- Practice/Chapter3/vue.js
+++ Practice/Chapter3/vue.js
@@ -1,13 +1,3 @@
 new Vue({
-  el: '#app',
-  data: {
-    lists: [
-      '赤', '青', '黄'
-    ]
-  },
-  methods: {
-    onclick: function() {
-      this.lists.shift()
-    }
-  }
-});
+  el: '#app'
+})
