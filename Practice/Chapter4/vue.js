47dd07288533ffbeea0f6fa2d3d932ccab8f2afd
Kosan3 <ichiro.neko.0421@gmail.com>
2020-09-30 19:14:45 +0900

    153

--- Practice/Chapter4/vue.js
+++ Practice/Chapter4/vue.js
@@ -1,13 +1,29 @@
-let myHello = {
-  props: ['yourName'],
+let myCount = {
   template: `
-    <div>こんにちは、{{ yourName }}さん!</div>
-  `
+    <div>
+      <button @click="onclick">クリック</button>
+      <p>{{ count }}回</p>
+      <button @click="reset">リセット</button>
+    </div>
+  `,
+  data: function() {
+    return {
+      count: 0
+    }
+  },
+  methods: {
+    onclick: function() {
+      this.count += 1;
+    },
+    reset: function() {
+      this.count = 0;
+    }
+  }
 };
 
 new Vue({
+  el: '#app',
   components: {
-    'my-hello': myHello
-  },
-  el: '#app'
+    'my-count': myCount
+  }
 })
