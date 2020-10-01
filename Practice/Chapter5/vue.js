43b83074963014c911def9f603e7e96df451a5ba
Kosan3 <ichiro.neko.0421@gmail.com>
2020-10-01 11:11:24 +0900

    183

--- Practice/Chapter5/vue.js
+++ Practice/Chapter5/vue.js
@@ -1,19 +1,34 @@
-let bannerMember = {
+let tabMember = {
   template: `
-    <div class="banner">
-      <h3>WINGSメンバー募集中！</h3>
-      <p>プロジェクトに参加しませんか？<br>
-      現在、募集中です。</p>
+    <div class="tab">
+      <p>あなたもWINGSプロジェクトに参加しませんか？<br>
+      現在、WINGSプロジェクトは、ご一緒にお仕事ができる仲間を募集中です。</p>
+      <label>名前：<input type="text" v-model="name"></label>
+      <input type="submit" value="登録">
+    </div>
+  `,
+  data: function() {
+    return {
+      name: ''
+    }
+  }
+};
+
+let tabNew = {
+  template: `
+    <div class="tab">
+      <h3>「HTML5超入門」発売！</h3>
+      <p>sステップバイステップで学ぶ入門書です。<br>
+      HTML5アプリの基礎知識、画面のデザイン。。。</p>
     </div>
   `
 };
 
-let bannerNew = {
+let tabEnv = {
   template: `
-    <div class="banner">
-      <h3>新刊「HTML5超入門 」発売</h3>
-      <p>ステップバイステップで学ぶ入門書です。<br />
-        HTML5アプリの基礎知識、画面のデザイン、コードの書き方などが理解できます。</p>
+    <div class="tab">
+      <p>開発環境の設定方法を図を交えて詳しく解説します。<br>
+      さあ、あなたもやりましょうよ</p>
     </div>
   `
 };
@@ -21,25 +36,38 @@ let bannerNew = {
 new Vue({
   el: '#app',
   components: {
-    'banner-member': bannerMember,
-    'banner-new': bannerNew
+    'tab-member': tabMember,
+    'tab-new': tabNew,
+    'tab-env': tabEnv
   },
-  created: function() {
-    let that = this;
-    this.interval = setInterval(function() {
-      that.current = (that.current + 1) % that.components.length;
-    }, 3000);
-  },
-  beforeDestroy: function() {
-    clearInterval(this.interval);
+  methods: {
+    onclick: function(tab) {
+      console.log(tab);
+      this.current = tab;
+    }
   },
   computed: {
-    currentBanner: function() {
-      return 'banner' + this.components[this.current];
+    tabNames: function() {
+      return Object.keys(this.tabs);
+    },
+    currentTab: function() {
+      return 'tab-' + this.current;
     }
   },
   data: {
-    current: 0,
-    components: ['member', 'new']
+    current: 'member',
+    tabs: {
+      'member': 'メンバー募集',
+      'new': '新刊紹介',
+      'env': '環境構築設定'
+    }
   }
 });
+
+let obj = {
+  'member': 'メンバー募集',
+  'new': '新刊紹介',
+  'env': '環境構築設定'
+};
+
+console.log(Object.keys(obj));
